dfa0bbcbe1eb4861479b69e0362495a72a4e992b
hudaazeem440 <156990131+hudaazeem440@users.noreply.github.com>
2024-01-19 10:42:32 -0800

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,14 @@
+num1=int(input("enter first number here: "))
+num2=int(input("enter second number here: "))
+print("press 1 for addition \npress 2 for subtraction \npress 3 for multiplication \npress 4 for division")
+choice= int(input("enter your choice from 1-4: "))
+if choice==1:
+    print(num1 + num2)
+elif choice==2:
+    print(num1 - num2)
+elif choice==3:
+    print(num1 * num2)
+elif choice==4:
+    print(num1 / num2)
+else:
+    print("invalid input")
